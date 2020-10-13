e71093adb6663364333115caca79565abdef0229
Warren <warreny7853@gmail.com>
2020-10-12 21:46:14 -0700

    Add files via upload

--- Frame_rigidElem.m
+++ Frame_rigidElem.m
@@ -8,7 +8,7 @@ clear;
 close all;
 
 % num of towers
-twr = 2;    % for test
+twr = 4;
 
 % Define global parameters for Frame (single tower)
 nsd = 3;        % number of spatial dimensions 
@@ -16,13 +16,20 @@ nel = 6;        % number of elements
 nnp = 6;        % number of nodes
 ndf = 7;        % number of degrees of freedom (doesn't change, ever!)
 
+% init bridge dimensions
+dhs = 381;      % distance to half span (dhs) [m]
+rel = 1;        % rigid element length (rel) [m]
+hod = -54.25;   % height of deck, where deck is at 0m (hod) [m] 
+wod = 24;       % width of deck, maximum width of tower (wod) [m]
+hot = 80;       % max height of tower (hot) [m]
+
 % Nodal definitions
 xn = zeros(nnp,nsd);	% xyz nodal coordinates [m]
 
 % (right top) w/ rigid elems
-xn = [381,12,-54.25; 
-      381,24,0; 381,23,0; 381,1,0; 381,0,0;
-      381,12,80];  
+xn = [dhs,wod/2,hod; 
+      dhs,wod,0; dhs,wod-rel,0; dhs,rel,0; dhs,0,0;
+      dhs,wod/2,hot];  
   
 % Supported Dofs
 idb = zeros(nnp,ndf);	% index of dofs - supported
@@ -57,23 +64,17 @@ elseif twr == 2 % for two towers
     
     % nodes are double (what about the middle node? it's not a hinge!)
     % elements double
-    nnp2 = 2*nnp;
-    nel2 = 2*nel;
-    
-    % create y-translating vector
-    y = zeros(nnp,nsd);
-    y(:,2) = 24;
-    
-%     size(y)
-%     size(xn)
+    nnp = 2*nnp;
+    nel = 2*nel;
     
     % translate and concatenate for second tower set of nodes
-    xn2 = xn - y;
+    xn2 = xn;
+    xn2(:,2) = -xn(:,2);
     xn = [xn; xn2];
     
     % init new ien for more towers
-    ien = [nel2,2];
-    ien = [1 2; 1 5; 2 3; 2 6; 4 5; 5 6; 5 8;  
+    ien = [nel,2];
+    ien = [1 2; 1 5; 2 3; 2 6; 4 5; 5 6;  
            7 8; 7 11; 8 9; 8 12; 10 11; 11 12];
        
     prop = zeros(nel,16);	% element properties
@@ -91,23 +92,34 @@ elseif twr == 2 % for two towers
 elseif twr == 4 % for all four towers
     % nodes are x 4
     % elements x 4
-    nnp4 = 4*nnp;
-    nel4 = 4*nel;
-    
-    % create y-translating vector
-    y = zeros(nnp,nsd);
-    y(:,2) = 24;
+    nnp = 4*nnp;
+    nel = 4*nel;
     
     % translate and concatenate for second tower set of nodes
-    xn2 = xn - y;
-    xn4 = [ -xn(:,1), xn(:,2:3);-xn2(:,1), xn2(:,2:3)]; 
-    xn = [xn; xn2; x4];
+    % y translation
+    xn2 = xn;
+    xn2(:,2) = -xn(:,2);
+    
+    % concatenate translated tower
+    xn = [xn; xn2];
+    
+    % x translation
+    xn4 = xn; 
+    xn4(:,1) = -xn(:,1);
+    
+    % concatenate translated tower
+    xn = [xn; xn4];
     
     % init new ien for more towers
-    ien = [nel4,2];
-    ien = [1 2; 1 5; 2 3; 2 6; 4 5; 5 6; 5 8;  
-           7 8; 7 11; 8 9; 8 12; 10 11; 11 12];
+    ien1 = [1 2; 1 5; 2 3; 2 6; 4 5; 5 6];
+    ien2 = ien1 + 6;
+    ien3 = ien2 + 6;
+    ien4 = ien3 + 6;
+    
+    ien = [nel,2];
+    ien = [ien1;ien2;ien3;ien4];
        
+    % this still needs fixing!
     prop = zeros(nel,16);	% element properties
     prop(:,1) = 3;	% element type [3 = frame]
     prop(:,2) = E;	% E modulus [N/mm^2]
